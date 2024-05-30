e12e27eabe7c0198207f268d560f1427aebe884d
thiagooshiro <oshirothiago@gmail.com>
2024-05-30 18:29:14 -0300

    initial commit

---
+++ projeto/src/components/footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Footer() {
+    return(
+        <nav>
+            <img src=''/>
+            <div>
+            	<p>Oshiro 2024</p>
+            </div>
+        </nav>
+    )
+}
+
+
+export default { Footer }
---
+++ projeto/src/components/header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Header() {
+    return(
+        <nav>
+            <img src=''/>
+            <div>
+            	<input placeholder='email'></input>
+            	<input placeholder='senha'></input>
+            </div>
+        </nav>
+    )
+}
+
+
+export default { Header }
