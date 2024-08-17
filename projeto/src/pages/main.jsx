80c070dc1205209aafc2390707a6b58e53fefc27
thiagooshiro <oshirothiago@gmail.com>
2024-08-17 17:41:33 -0300

    atualiza sincado com o repo ou não

--- projeto/src/pages/main.jsx
+++ projeto/src/pages/main.jsx
@@ -1,25 +1,10 @@
-import React, { useEffect, useContext } from 'react'
+import React from 'react'
 import './main.css'
 import Footer from '../components/footer'
 import Header from '../components/header'
-import { UserContext } from '../context/userContext'
-import Profile from '../didatico/aula2/aula2'
-import ExemploComponente from '../didatico/aula3/exemploComponente'
 import LoginForm from '../components/loginform'
 
 function Main() {
-	const {pessoas, setPessoas} = useContext(UserContext)
-
-	// useEffect(() => { 
-	// 	const fetchUsers = async () => {
-	// 		const response = await fetch('/pessoas.json')
-	// 			const data = await response.json();
-	// 		  	setPessoas(data);
-	// 	  };
-	// 	fetchUsers();
-	// 	}, [])
-
-		
 
 	return(
 		<div>
